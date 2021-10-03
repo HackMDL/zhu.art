579d9820ca59bb7651ce860abd676d0333bd84b1
HackMDL <32384002+HackMDL@users.noreply.github.com>
2021-10-03 22:09:54 +0300

    beta01

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        fade: true,
+    });
+});
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $('#scroll_top').show();
+        } else {
+            $('#scroll_top').hide();
+        }
+    });
+
+    $('#scroll_top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
